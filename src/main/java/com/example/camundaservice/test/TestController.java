f5bc96b762b9fffb9c194807de38ef89f98f2242
mike-bychkov <mixail.b@halykmarket.com>
2024-03-04 11:02:38 +0500

    camunda api

--- src/main/java/com/example/camundaservice/test/TestController.java
+++ src/main/java/com/example/camundaservice/test/TestController.java
@@ -1,5 +1,8 @@
 package com.example.camundaservice.test;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.impl.RuntimeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    @Autowired
+    private RuntimeService runtimeService;
+
     @GetMapping("/hello")
     public ResponseEntity<String> getHello() {
+
+//        runtimeService.restartProcessInstances()
 
         return ResponseEntity.ok("Hello");
     }
